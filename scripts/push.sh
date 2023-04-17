81892b9dea764f94791474e491481643a2e9a310
Ruslan Islanbekov <ruslan.islanbekov@dualbootpartners.com>
2023-04-17 16:27:52 +0600

    completed lesson 11

--- scripts/push.sh
+++ scripts/push.sh
@@ -3,7 +3,7 @@ curl \
 -H "Authorization:key=AAAA4BHW5Dw:APA91bH8AYp83gSU9UN690hPUiFRQn-v6kNGo0qYR98M-FEkMvf1B679LQZFzzPKYgW6EZlFMISgQMlBr_O-H0XfiuuhCvIXGKdVYmqNPBUvy9szao7CCnE0CBOq5HaOC0ogLXc6ETxl" \
 -X POST \
 -d '{
-"to" : "d1LcqYhFREOYb3S6T8hVqM:APA91bF56k67itYcv8k19BOfSNxmNHs4jqPAPsVbjkYgHq1TmQ_n8tKakOgYo9kMS7u4nWV89Zs1xn6r90DFSJJO5_rI4TOkBU3cHBoY88fiLeHEljPJmry0Y3tm4mos926pxzve9SPJ"
+"to" : "fwx48SjBRuanpYlgpfuZVI:APA91bEtYjGT9-ZnhazBUQeZ1lNmmSb7hdqEwkB2mqFmhlvFSMNh9BEhjVSTat1btESM4B4bYjR_O0tLeWprFOAo1tdu4x4IiNSH3D6HhBo0eqs9w0WXsM_GPM4oeoOEXJRFq6S2Gy3P"
 "notification":{
 "title":"Hello",
 "body":"This is a text message!"
